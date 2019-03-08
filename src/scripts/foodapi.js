c8551ad2b0058ec3b5b9bf954d504bd5401a4143
Jameka Echols <jameka.echols@gmail.com>
2019-03-07 21:35:30 -0600

    forgot to make another branch, added fetch calls to add to the dom and created the css

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,9 +1,25 @@
 console.log("linked")
+// created var that will get the body in which the foods will be displayed in
+const entriesOnDom = document.querySelector('#foodlist')
+
+// creating a component that will be called in the fetch that will display all the items on to the dom 
+const htmlOnDom = (foodsContainer) => {
+    return `
+        <div class="foodItems">
+            <h3>${foodsContainer.name}</h3>
+            <p>${foodsContainer.category}</p>
+            <p>${foodsContainer.ethnicity}</p>
+        </div>
+    `
+}
+
 
 fetch("http://localhost:8088/foods")
     .then(foods => foods.json())
     .then(parsedFoods => {
-        console.table(parsedFoods)
+        parsedFoods.forEach(item => {
+            entriesOnDom.innerHTML += htmlOnDom(item)
+        })
     })
 
-const entriesOnDom = document.querySelector('#foodlist')
+
